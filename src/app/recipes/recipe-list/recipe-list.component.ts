e2e7a428d6f757f24e5968a327443e9ac83743d6
jesse-ph <jesse.e.eclevia@gmail.com>
2020-09-06 22:56:48 +0800

    section 3

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'sample description', 'https://t3.ftcdn.net/jpg/01/22/73/00/240_F_122730057_ap199rB09c14HYYHfQRr8JFRPwLNLGRl.jpg')
+    , new Recipe('A Test Recipe', 'sample description', 'https://t3.ftcdn.net/jpg/01/22/73/00/240_F_122730057_ap199rB09c14HYYHfQRr8JFRPwLNLGRl.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
