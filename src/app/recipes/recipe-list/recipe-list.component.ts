0376fd4f9d95c31e135aa900e78c0753f0d3c209
jesse-ph <jesse.e.eclevia@gmail.com>
2020-09-29 18:53:12 +0800

    section 6 and 8

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'sample description', 'https://t3.ftcdn.net/jpg/01/22/73/00/240_F_122730057_ap199rB09c14HYYHfQRr8JFRPwLNLGRl.jpg')
-    , new Recipe('A Test Recipe', 'sample description', 'https://t3.ftcdn.net/jpg/01/22/73/00/240_F_122730057_ap199rB09c14HYYHfQRr8JFRPwLNLGRl.jpg')
+    new Recipe('A Test Recipe', 'This is a simple a test', 'https://t3.ftcdn.net/jpg/01/22/73/00/240_F_122730057_ap199rB09c14HYYHfQRr8JFRPwLNLGRl.jpg')
+    , new Recipe('Another Test Recipe', 'This is simply a test', 'https://t3.ftcdn.net/jpg/01/22/73/00/240_F_122730057_ap199rB09c14HYYHfQRr8JFRPwLNLGRl.jpg')
   ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
